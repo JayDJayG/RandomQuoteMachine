b82e3c7db1516aa64c22d629ba4aca064b7cc55d
Jay Guzman <j.d.guzman.v@gmail.com>
2018-02-02 09:33:49 -0700

    Fetching the JSON quote, WORKING STATUS = FINE

---
+++ randomQuoteFetch.js
@@ -0,0 +1,16 @@
+// Let's get the Random quote First
+let url = 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=';
+
+let request = new Request(url, {
+   headers : new Headers({
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'X-Mashape-Key': 'x4KwcPRjkbmshOPgAiWYwr0B4f7vp1wrN5rjsnwMAckYt6AGC4'
+  })
+});
+
+fetch(request).then(function(response){
+  response.json().then(function(data){
+    if (data)console.log("Good", data);
+    else console.log("Freek");
+  });
+});
