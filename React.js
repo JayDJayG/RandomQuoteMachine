e94588138c19ce5b6b267833c15c331eeee1a218
Jay Guzman <j.d.guzman.v@gmail.com>
2018-02-04 10:24:57 -0700

    componentDidMount() added to the Class Main, it fetches succesfully the data and changes the states of the class Main allowing to display the data received

--- React.js
+++ React.js
@@ -1,17 +1,34 @@
 // ReactDOM
+const url = 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=';
+    const request = new Request(url, {
+      headers : new Headers({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'X-Mashape-Key': 'x4KwcPRjkbmshOPgAiWYwr0B4f7vp1wrN5rjsnwMAckYt6AGC4'
+        })
+      });
 
+// ReactDOM
 class Main extends React.Component {
   constructor(props) {
-    super(props);
+    super(props );
     this.state = {
-      value: null,
+      quote : ["Failed Loading"],
+      author : [],
     };
   }
+   componentDidMount() {
+    fetch(request)
+      .then(response => response.json())
+      .then(data => this.setState({ quote: data.quote, author : data.author }));
+   }
+
 
   render() {
     return (
       <div>
-        <h1>Random Quote Machine</h1>
+        <h2 className= "">Random Quote Machine</h2>
+        <h1>{this.state.quote}</h1>
+        <h3>"{this.state.author}"</h3>
       </div>
     );
 
