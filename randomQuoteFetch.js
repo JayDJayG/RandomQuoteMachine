c6737d0f1b597ae15256774f1b78db8a7f7082ae
Jay Guzman <j.d.guzman.v@gmail.com>
2018-02-04 09:38:37 -0700

    Added to the file randomQuoteFetch.js, the right place where it should go inside the React component Lyfecicle; changed the syntax from ES5 to ES6

--- randomQuoteFetch.js
+++ randomQuoteFetch.js
@@ -1,18 +1,19 @@
 // Let's get the Random quote First
-let url = 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=';
+const url = 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=';
 
-let request = new Request(url, {
+const request = new Request(url, {
    headers : new Headers({
   'Content-Type': 'application/x-www-form-urlencoded',
   'X-Mashape-Key': 'x4KwcPRjkbmshOPgAiWYwr0B4f7vp1wrN5rjsnwMAckYt6AGC4'
   })
 });
 
-fetch(request).then(function(response){
-  response.json().then(function(data){
-    if (data)console.log("Good", data);
-    else console.log("Freek");
-  });
-});
+//componentDidMount() has to be placed inside the React class to change the State
+
+componentDidMount() {
+    fetch(request)
+      .then(response => response.json())
+      .then(data => this.setState({ quote: data.quote, author : data.author }));
+   }
 
 //Working Just fine
